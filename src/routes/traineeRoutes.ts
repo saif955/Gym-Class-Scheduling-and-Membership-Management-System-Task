fe865662bac5d8eeddf0a779a4702c4e6af1840d
Saifur Rahman <99525405+saif955@users.noreply.github.com>
2025-04-12 12:57:18 +0600

    Done

--- src/routes/traineeRoutes.ts
+++ src/routes/traineeRoutes.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { enrollSchedule, getProfile, updateProfile,deleteProfile } from "../controllers/traineeController";
+import { getAllSchedules } from "../controllers/scheduleController";
 import { authenticateUser, authorizeAdmin,authorizeTrainee } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 router.post('/enroll', authenticateUser, authorizeTrainee, enrollSchedule);
 router.get('/profile', authenticateUser, authorizeTrainee, getProfile);
+router.get('/schedules', authenticateUser, authorizeTrainee, getAllSchedules);
 router.put('/profile', authenticateUser, authorizeTrainee, updateProfile);
 router.delete('/profile', authenticateUser, authorizeTrainee, deleteProfile);
 export default router;
